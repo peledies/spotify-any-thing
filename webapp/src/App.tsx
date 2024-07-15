6787b6ee296da3497526118ba86c5e532870155e
Nathan Stanley <nstanley@bandwidth.com>
2024-07-15 17:52:48 -0400

    react-app: progress bar

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,64 +1,75 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ButtonLabel from "./components/ButtonLabel";
-import { Button, ButtonCodeMap } from "./utils/ButtonHelper";
+import { Button, useKeyDown, useKeyUp, useWheel } from "./utils/ButtonHelper";
 
 export default function App() {
-  const [labelState, setLabelState] = useState<Record<Button, boolean>>({
+  const [labelState, setLabelState] = useState<
+    Partial<Record<Button, boolean>>
+  >({
     [Button.BUTTON_1]: false,
     [Button.BUTTON_2]: false,
     [Button.BUTTON_3]: false,
     [Button.BUTTON_4]: false,
   });
 
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyDown);
-    document.addEventListener("keyup", handleKeyup);
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-      document.removeEventListener("keyup", handleKeyup);
-    };
-  }, []);
+  const [progress, setProgress] = useState(0);
 
-  const handleKeyDown = (e: KeyboardEvent) => {
-    e.preventDefault();
-    const button = ButtonCodeMap.get(e.code);
-    if (button === undefined) return;
-    setLabelState({ ...labelState, [button]: true });
-  };
+  useKeyDown({
+    [Button.BUTTON_1]: () =>
+      setLabelState({ ...labelState, [Button.BUTTON_1]: true }),
+    [Button.BUTTON_2]: () =>
+      setLabelState({ ...labelState, [Button.BUTTON_2]: true }),
+    [Button.BUTTON_3]: () =>
+      setLabelState({ ...labelState, [Button.BUTTON_3]: true }),
+    [Button.BUTTON_4]: () =>
+      setLabelState({ ...labelState, [Button.BUTTON_4]: true }),
+  });
 
-  const handleKeyup = (e: KeyboardEvent) => {
-    e.preventDefault();
-    const button = ButtonCodeMap.get(e.code);
-    if (button === undefined) return;
-    setLabelState((prev) => {
-      return { ...labelState, [button]: false };
-    });
-  };
+  useKeyUp({
+    [Button.BUTTON_1]: () =>
+      setLabelState({ ...labelState, [Button.BUTTON_1]: false }),
+    [Button.BUTTON_2]: () =>
+      setLabelState({ ...labelState, [Button.BUTTON_2]: false }),
+    [Button.BUTTON_3]: () =>
+      setLabelState({ ...labelState, [Button.BUTTON_3]: false }),
+    [Button.BUTTON_4]: () =>
+      setLabelState({ ...labelState, [Button.BUTTON_4]: false }),
+  });
+
+  useWheel((direction) => {
+    const newProgress = direction === "right" ? progress + 1 : progress - 1;
+    setProgress(newProgress < 0 ? 0 : newProgress > 100 ? 100 : newProgress);
+  });
 
   return (
-    <div
-      className="border border-white box-border relative"
-      style={{ height: 480, width: 800 }}
-    >
+    <div className="relative" style={{ height: 480, width: 800 }}>
+      <div className="absolute top-[50px] left-0 right-0 bottom-0">
+        <div className="w-3/4 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+          <div
+            className="bg-blue-600 h-2.5 rounded-full"
+            style={{ width: `${progress}%` }}
+          ></div>
+        </div>
+      </div>
       <ButtonLabel
         label="Button 1"
         button={Button.BUTTON_1}
-        buttonPressed={labelState[Button.BUTTON_1]}
+        buttonPressed={labelState[Button.BUTTON_1] ?? false}
       />
       <ButtonLabel
         label="Button 2"
         button={Button.BUTTON_2}
-        buttonPressed={labelState[Button.BUTTON_2]}
+        buttonPressed={labelState[Button.BUTTON_2] ?? false}
       />
       <ButtonLabel
         label="Button 3"
         button={Button.BUTTON_3}
-        buttonPressed={labelState[Button.BUTTON_3]}
+        buttonPressed={labelState[Button.BUTTON_3] ?? false}
       />
       <ButtonLabel
         label="Button 4"
         button={Button.BUTTON_4}
-        buttonPressed={labelState[Button.BUTTON_4]}
+        buttonPressed={labelState[Button.BUTTON_4] ?? false}
       />
     </div>
   );
--- webapp/src/utils/ButtonHelper.ts
+++ webapp/src/utils/ButtonHelper.ts
@@ -1,8 +1,11 @@
+import { useEffect } from "react";
+
 export enum Button {
   BUTTON_1,
   BUTTON_2,
   BUTTON_3,
   BUTTON_4,
+  BUTTON_5,
 }
 
 export const ButtonLabelPositions = new Map([
@@ -17,7 +20,67 @@ export const ButtonCodeMap = new Map([
   ["Digit2", Button.BUTTON_2],
   ["Digit3", Button.BUTTON_3],
   ["Digit4", Button.BUTTON_4],
-  // ["KeyM", Button.BUTTON_5],
+  ["KeyM", Button.BUTTON_5],
   // ["Enter", Button.SCROLL_PRESS],
   // ["Escape", Button.FRONT_BUTTON],
 ]);
+
+export const useKeyDown = (callbacks: Partial<Record<Button, () => void>>) => {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      e.preventDefault();
+      const button = ButtonCodeMap.get(e.code);
+      if (button === undefined) return;
+      callbacks[button]?.();
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+};
+
+export const useKeyUp = (callbacks: Partial<Record<Button, () => void>>) => {
+  useEffect(() => {
+    const handleKeyUp = (e: KeyboardEvent) => {
+      e.preventDefault();
+      const button = ButtonCodeMap.get(e.code);
+      if (button === undefined) return;
+      callbacks[button]?.();
+    };
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  });
+};
+
+// NOTE: Not implemented, but could be used in addition to or instead of useKeyDown and useKeyUp
+export const useKeyPress = (callbacks: Partial<Record<Button, () => void>>) => {
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent) => {
+      e.preventDefault();
+      const button = ButtonCodeMap.get(e.code);
+      if (button === undefined) return;
+      callbacks[button]?.();
+    };
+    document.addEventListener("keypress", handleKeyPress);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  });
+};
+
+export const useWheel = (callback: (direction: "left" | "right") => void) => {
+  useEffect(() => {
+    const handleWheel = (e: WheelEvent) => {
+      e.preventDefault();
+      const right = e.deltaX > 0;
+      callback(right ? "right" : "left");
+    };
+    document.addEventListener("wheel", handleWheel);
+    return () => {
+      document.removeEventListener("wheel", handleWheel);
+    };
+  });
+};
